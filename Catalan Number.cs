924d87eacb570a8258ba5ccea38834d21827448e
Onur Yartaşı <onuryartasi@live.com>
2016-10-13 21:39:34 +0300

    Calatan Number
    The easiest solution way to this problem

---
+++ Catalan Number.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+namespace Catala_Number
+{
+    class MainClass
+    {
+   
+        public static int KatalanSayisi(int n)
+		{ int ans = 0;
+            
+          
+            if (n < 0)
+            {
+                return -1;
+            }
+            else if (n == 0)
+            {
+                return 1;
+            }
+            else if (n == 1) { return 1; }
+            {
+                    ans += KatalanSayisi(i) * KatalanSayisi(n - i - 1);
+            }
+            
+
+
+            return ans;
+
+        }
+
+		
+			
+		public static void Main(string[] args)
+        {
+           
+            Console.WriteLine(KatalanSayisi(10));
+            
+
+            Console.ReadLine();
+		}
+	}
+}
+
